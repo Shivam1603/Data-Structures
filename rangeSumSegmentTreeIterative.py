4f331761665c3b0ee4de1a31623eb45d2d978ab8
Shivam1603 <53405971+Shivam1603@users.noreply.github.com>
2022-05-14 20:58:26 +0530

    Create rangeSumSegmentTreeIterative.py
    Iterative implementation of range sum segment tree in python

---
+++ rangeSumSegmentTreeIterative.py
@@ -0,0 +1,40 @@
+class SegTree(object):
+    '''
+        0 based indexing
+        includes both left and right indices
+    '''
+    def __init__(self, nums):
+        self.l = len(nums)
+        self.tree = [0] * self.l + nums
+        for i in range(self.l - 1, 0, -1):
+            self.tree[i] = self.tree[i << 1] + self.tree[i << 1 | 1]
+
+    def update(self, i, val):
+        n = self.l + i
+        self.tree[n] = val
+        while n > 1:
+            self.tree[n >> 1] = self.tree[n] + self.tree[n ^ 1]
+            n >>= 1
+
+    def sumRange(self, i, j):
+        m = self.l + i
+        n = self.l + j
+        res = 0
+        while m <= n:
+            if m & 1:
+                res += self.tree[m]
+                m += 1
+            m >>= 1
+            if n & 1 == 0:
+                res += self.tree[n]
+                n -= 1
+            n >>= 1
+        return res
+
+tree = SegTree([1, 2, 3, 4, 5, 6])
+
+print(tree.sumRange(0, 5))  #prints 21
+
+tree.update(1, 5) #updates the 2nd element (index = 1) to 5
+
+print(tree.sumRange(0, 5))  #prints 24
